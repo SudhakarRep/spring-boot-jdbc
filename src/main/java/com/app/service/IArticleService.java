cae663f415de71331cdb087ee1e5327632d5929c
Sudhakar Avisa <sudhakar.avise@netenrich.com>
2019-05-27 20:40:47 +0530

    Spring boot with JDBC Template example

---
+++ src/main/java/com/app/service/IArticleService.java
@@ -0,0 +1,13 @@
+package com.app.service;
+
+import java.util.List;
+
+import com.app.entity.Article;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}
